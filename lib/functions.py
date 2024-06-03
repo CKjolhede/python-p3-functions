e8f9db055fb5ca768f905d6564c082ec9e1436b5
ckjolhede <ckjolhede@gmail.com>
2024-06-03 00:37:05 -0600

    complete

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+    return
 
 def greet(name):
-    pass
+    name = "Guido"
+    print(f"Hello, {name}!")
+    return  
 
 def greet_with_default(name="programmer"):
+    print(f"Hello, {name}!")
     pass
 
 def add(num1, num2):
+    num1 = int(num1)
+    num2 = int(num2)
+    return num1 + num2
     pass
 
 def halve(number):
+    number = int(number) / 2
+    return number
     pass
